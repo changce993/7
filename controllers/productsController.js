6d02c584cf6e91148432cf1e08af97b987f08804
Ricardo Chance <changcer@gmail.com>
2021-02-02 21:03:00 -0300

    ...

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,14 +2,16 @@ const Products = require('../db/products.json');
 
 module.exports = {
     items: (req,res) => {
-        req.session.items += 1;
+        req.session.items ? req.session.items++ : req.session.items = 1
+
         res.send({
             items: Products,
             cantidad: Products.length
         });
     },
     itemRandom: (req,res) => {
-        req.session.itemRandom += 1;
+        req.session.itemRandom ? req.session.itemRandom++ : req.session.itemRandom = 1;
+
         const idRandom = Math.floor(Math.random() * (Products.length - 1) + 1);
         const product = Products.find(product => product.id === idRandom);
         res.send(product);
--- index.js
+++ index.js
@@ -3,15 +3,14 @@ const app = express();
 const session = require('express-session');
 const productsRoute = require('./routes/products');
 const viewsRoute = require('./routes/views');
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 
 app.use(session({
     secret: 'Oeee!!',
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: true }
-}))
+}));
 app.use( express.json({extend : true}));
 app.use('/', productsRoute);
-app.use('/visitas', viewsRoute);
-app.listen(PORT, () => console.log(`Server in port: ${PORT}`)); 
+app.use('/views', viewsRoute);
+app.listen(PORT, () => console.log(`Server on port: ${PORT}`));
