e2fd8aea065d7fd27adf5785262c50e0f3cede9a
changce993 <changcer@gmail.com>
2021-02-23 22:09:21 -0300

    ejs

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const expshbs = require('express-handlebars');
 const session = require('express-session');
 const apiRoute = require('./routes/api');
 const methodOverride = require('method-override');
@@ -16,11 +15,13 @@ app.use(session({
     saveUninitialized: false,
 }));
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(methodOverride());
 app.use( express.json({extend : true}));
 app.use(express.static('public'))
 app.use('/api', apiRoute);
+app.use('/', (req,res) => res.render('index'));
+app.use((req,res) => res.status(404).render('404'));
 
 app.listen(PORT, () => console.log('Server on port:', PORT)).on('error', err => console.log('Error de conexion:',err));
